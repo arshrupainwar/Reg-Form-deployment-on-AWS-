928e99bd07f382464b6a1f571dbae8a5acebd52e
arshrupainwar <arshrupainwar@gmail.com>
2021-11-11 09:37:34 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cq970ldmaxtv.ap-south-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "emp12345"
+customdb = "employee"
+custombucket = "epm-project-2021"
+customregion = "ap-south-1"
